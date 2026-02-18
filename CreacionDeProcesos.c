10cd0694e230b6e523392c98f90b002443a47e50
Ale Colque <95255576+theale18@users.noreply.github.com>
2026-02-17 23:37:34 -0300

    Update CreacionDeProcesos.c

--- CreacionDeProcesos.c
+++ CreacionDeProcesos.c
@@ -1,18 +1,30 @@
-#include <sys/types.h> 
-#include <unistd.h> 
-#include <stdio.h> 
+#include <sys/types.h>  // define tipos como pid_t (tipo de dato para IDs de procesos). 
+#include <unistd.h>     // contiene funciones del sistema Unix como fork(), getpid(), getppid().
+#include <stdio.h>      // printf()
  
 int main() 
 { 
-    pid_t pid; 
+    pid_t pid;      // Se declara pid, que almacenará el valor devuelto por fork().
     pid = fork(); 
     if (pid == 0) 
     { /* hijo */ 
         printf("Soy el hijo (%d, hijo de %d)\n", getpid(), getppid()); 
+//  getpid() → PID del proceso actual (el hijo)
+//  getppid() → PID del padre del proceso (el padre)    
     } 
     else 
     { /* padre */ 
         printf("Soy el padre (%d, hijo de %d)\n", getpid(), getppid()); 
     } 
     return 0; 
-} 
+}
+
+/*
+Supongamos que el PID del padre es 1000
+y el del hijo 1001.
+
+La salida podría ser:
+Soy el padre (1000, hijo de 950)
+Soy el hijo (1001, hijo de 1000)
+
+*/
