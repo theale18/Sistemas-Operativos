ffe6e982fe84fd2d8ab4f9d2ca620008f9b37891
Ale Colque <95255576+theale18@users.noreply.github.com>
2026-02-17 23:01:50 -0300

    Create MemoriaCompartida.c

---
+++ MemoriaCompartida.c
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+#include<sys/shm.h> 
+#include<sys/stat.h> 
+ 
+#define TAM 1024 
+ 
+int main() 
+{ 
+    int segment_id; // un id del Segmento de Memoria compartida 
+    char *shared_memory; // un puntero a la Memoria Compartida 
+    //obtener un ID del segmento de memoria 
+    segment_id = shmget(1981, TAM, 0664|IPC_CREAT); 
+     
+    if(segment_id == -1) { //si surge algun problema 
+        fprintf(stderr,"Error al reservar la memoria"); 
+    } 
+    //obtener un puntero al Segmento de memoria Compartida 
+    shared_memory = (char *) shmat(segment_id, NULL, 0); 
+    sprintf(shared_memory, "HOLA!"); //Escribir en el Shared Memory 
+    //Enviar a la Pantalla lo que hay en la memoria compartida 
+    printf("%s\n", shared_memory); 
+    shmdt(shared_memory); //Desconectar Shared Memory 
+    shmctl(segment_id, IPC_RMID,NULL); //Eliminar el espacio de 
+    direcciones 
+    return 0; 
+} 
